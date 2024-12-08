36b47f888a957e317601de0c70fb162107542d4e
Javadog <javamariodog@gmail.com>
2024-12-08 16:14:24 -0500

    custom region support

--- Hooks.cs
+++ Hooks.cs
@@ -28,30 +28,35 @@ namespace WDTGG
                 //Gets current region identifier Example: SU
                 string region = self.room.world.name;
 
+                //Gets slugcat specific regions Example instead of SS: Five Pebbles it will be RW: The Rot for Rivulet
+                //Doesn't realy do anything if the slugcat isn't Rivulet or Saint
+                region = Region.GetProperRegionAcronym(self.SlugCatClass, region);
+
                 //Is the new room a gate?
                 if (roomName.StartsWith("GATE"))
                 {
+                    
                     string nextRegion;
                     string[] splitRoomName = roomName.Split('_');
                     //Is the first element in the broken roomName is the region we're in
-                    if (splitRoomName[1] == region)
+                    if (Region.GetProperRegionAcronym(self.SlugCatClass, splitRoomName[1]) == region)
                     {
-                        //Set nextRegion to the desination region
-                        nextRegion = splitRoomName[2];
+                        //Set nextRegion to the desination region and get the correct acronym
+                        nextRegion = Region.GetProperRegionAcronym(self.SlugCatClass, splitRoomName[2]);
                     }
                     //If the first element is not the region we're in
                     else
                     {
-                        //Set nextRegion to the desination region
-                        nextRegion = splitRoomName[1];
+                        //Set nextRegion to the desination region and get the correct acronym
+                        nextRegion = Region.GetProperRegionAcronym(self.SlugCatClass, splitRoomName[1]);
                     }
+
 
                     //Gets slugcat specific regions Example instead of SS: Five Pebbles it will be RW: The Rot for Rivulet
                     string correctRegion = Region.GetProperRegionAcronym(self.SlugCatClass, nextRegion);
 
                     //Gets the full region name of the destination region
                     string regionFullName = Region.GetRegionFullName(correctRegion, self.SlugCatClass);
-
 
                     //Send message
                     self.room.game.cameras[0].hud.textPrompt.AddMessage("Gate to " + regionFullName, 0, 100, false, false);

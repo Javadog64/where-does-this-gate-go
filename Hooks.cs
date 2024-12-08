89a31e8b87773641d6344746af44fdd1806de636
Javadog <Javadog@NICECOMPUTER>
2024-12-07 22:13:22 -0500

    Add project files.

---
+++ Hooks.cs
@@ -0,0 +1,66 @@
+﻿using HUD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using IL.Smoke;
+using Steamworks;
+using System.Diagnostics;
+using UnityEngine;
+using System.IO;
+
+namespace WDTGG
+{
+    internal class Hooks
+    {
+        
+        public static void Apply()
+        {
+            On.Player.NewRoom += (orig, self, roomIN) =>
+            {
+                orig(self, roomIN);
+
+                //Gets current room name Example: GATE_SU_HI
+                string roomName = self.room.abstractRoom.name;
+                //Gets current region identifier Example: SU
+                string region = self.room.world.name;
+
+                //Is the new room a gate?
+                if (roomName.StartsWith("GATE"))
+                {
+                    string nextRegion;
+                    string[] splitRoomName = roomName.Split('_');
+                    //Is the first element in the broken roomName is the region we're in
+                    if (splitRoomName[1] == region)
+                    {
+                        //Set nextRegion to the desination region
+                        nextRegion = splitRoomName[2];
+                    }
+                    //If the first element is not the region we're in
+                    else
+                    {
+                        //Set nextRegion to the desination region
+                        nextRegion = splitRoomName[1];
+                    }
+
+                    //Gets the full region name of the destination region
+                    string regionFullName = Region.GetRegionFullName(nextRegion, self.SlugCatClass);
+
+
+                    //Send message
+                    self.room.game.cameras[0].hud.textPrompt.AddMessage("Gate to " + regionFullName, 0, 100, false, false);
+                }
+
+                
+                
+                
+            };
+        }
+
+
+
+    }
+}
---
+++ WDTGG.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+using IL;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace WDTGG;
+
+[BepInPlugin("javadog.gateNames", "Where Does This Gate Go?", "1.0.0")]
+public partial class WDTGG : BaseUnityPlugin
+{
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    private bool IsInit;
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        try
+        {
+            if (IsInit) return;
+
+            //Your hooks go here
+            Hooks.Apply();
+            
+
+            On.RainWorldGame.ShutDownProcess += RainWorldGameOnShutDownProcess;
+            On.GameSession.ctor += GameSessionOnctor;
+            
+            IsInit = true;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+            throw;
+        }
+    }
+    
+    private void RainWorldGameOnShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
+    {
+        orig(self);
+        ClearMemory();
+    }
+    private void GameSessionOnctor(On.GameSession.orig_ctor orig, GameSession self, RainWorldGame game)
+    {
+        orig(self, game);
+        ClearMemory();
+    }
+
+    #region Helper Methods
+
+    private void ClearMemory()
+    {
+        //If you have any collections (lists, dictionaries, etc.)
+        //Clear them here to prevent a memory leak
+        //YourList.Clear();
+    }
+
+    #endregion
+}

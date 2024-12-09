899c535063163df5b2561fb101f3508ebb267b62
Javadog <javamariodog@gmail.com>
2024-12-08 23:59:53 -0500

    i forgot to change the version oops

--- WDTGG.cs
+++ WDTGG.cs
@@ -17,7 +17,7 @@ using IL;
 
 namespace WDTGG;
 
-[BepInPlugin("javadog.gateNames", "Where Does This Gate Go?", "1.0.1")]
+[BepInPlugin("javadog.gateNames", "Where Does This Gate Go?", "1.0.2")]
 public partial class WDTGG : BaseUnityPlugin
 {
     private void OnEnable()

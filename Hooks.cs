168598fce78e96feee3d6e4153da76687353d019
Javadog <javamariodog@gmail.com>
2025-09-20 14:23:50 -0400

    redid hooks to be standard rather than lambda

--- Hooks.cs
+++ Hooks.cs
@@ -17,72 +17,70 @@ namespace WDTGG
 {
     internal class Hooks
     {
-        
+        // Here so that we can keep track of if 2 or more "Player"'s have entered a new room
+        private static int hasAnnounced = 0;
         public static void Apply()
         {
-            // Here so that we can keep track of if 2 or more "Player"'s have entered a new room
-            int hasAnnounced = 0;
-            On.Player.NewRoom += (orig, self, roomIN) =>
-            {
-                orig(self, roomIN);          
 
-               
-                //Gets current room name Example: GATE_SU_HI
-                string roomName = self.room.abstractRoom.name;
-                //Gets current region identifier Example: SU
-                string region = Region.GetVanillaEquivalentRegionAcronym(self.room.world.name);
+            On.Player.NewRoom += Player_NewRoom;
 
-
-                //Is the new room a gate?
-                if (self.room.IsGateRoom())
-                {
-                    
-                    //Has a player already entered the gate?
-                    if (hasAnnounced >= 1)
-                    {
-                        return;
-                    }
-
-                    //Increment counter
-                    hasAnnounced++;
-
-                    string nextRegion = "";
-                    string[] splitRoomName = roomName.Split('_');
-                    //Is the first element in the broken roomName is the region we're in
-                    if (splitRoomName[1] == region)
-                        {
-                        //Set nextRegion to the desination region and get the correct acronym
-                        nextRegion = splitRoomName[2];
-                    }
-                    //If the first element is not the region we're in
-                    else
-                    {
-                        //Set nextRegion to the desination region and get the correct acronym
-                        nextRegion = splitRoomName[1];
-                    }
-
-
-                    //Gets slugcat specific regions Example instead of SS: Five Pebbles it will be RM: The Rot for Rivulet
-                    string correctRegion = Region.GetProperRegionAcronym(self.SlugCatClass, nextRegion);
-
-                    //Gets the full region name of the destination region
-                    string regionFullName = Region.GetRegionFullName(correctRegion, self.SlugCatClass);
-
-                    //Send message
-                    self.room.game.cameras[0].hud.textPrompt.AddMessage(Custom.rainWorld.inGameTranslator.Translate("Gate to ") + Custom.rainWorld.inGameTranslator.Translate(regionFullName), 0, 100, false, true);
-                } else
-                {
-                    //PLayer has exited gate reset counter
-                    hasAnnounced = 0;
-                }
-
-                
-                
-                
-            };
         }
 
+        private static void Player_NewRoom(On.Player.orig_NewRoom orig, Player self, Room roomIn)
+        {
+            orig(self, roomIn);
 
+
+            //Gets current room name Example: GATE_SU_HI
+            string roomName = self.room.abstractRoom.name;
+            //Gets current region identifier Example: SU
+            string region = Region.GetVanillaEquivalentRegionAcronym(self.room.world.name);
+
+
+            //Is the new room a gate?
+            if (self.room.IsGateRoom())
+            {
+
+                //Has a player already entered the gate?
+                if (hasAnnounced >= 1)
+                {
+                    return;
+                }
+
+                //Increment counter
+                hasAnnounced++;
+
+                string nextRegion = "";
+                string[] splitRoomName = roomName.Split('_');
+                //Is the first element in the broken roomName is the region we're in
+                if (splitRoomName[1] == region)
+                {
+                    //Set nextRegion to the desination region and get the correct acronym
+                    nextRegion = splitRoomName[2];
+                }
+                //If the first element is not the region we're in
+                else
+                {
+                    //Set nextRegion to the desination region and get the correct acronym
+                    nextRegion = splitRoomName[1];
+                }
+
+
+                //Gets slugcat specific regions Example instead of SS: Five Pebbles it will be RM: The Rot for Rivulet
+                string correctRegion = Region.GetProperRegionAcronym(self.SlugCatClass, nextRegion);
+
+                //Gets the full region name of the destination region
+                string regionFullName = Region.GetRegionFullName(correctRegion, self.SlugCatClass);
+
+                //Send message
+                self.room.game.cameras[0].hud.textPrompt.AddMessage(Custom.rainWorld.inGameTranslator.Translate("Gate to ") + Custom.rainWorld.inGameTranslator.Translate(regionFullName), 0, 100, false, true);
+            }
+            else
+            {
+                //PLayer has exited gate reset counter
+                hasAnnounced = 0;
+            }
+        }
 
     }
 }
